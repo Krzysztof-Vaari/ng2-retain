1093ce42ac033d463347c3a4156bf6e8649a3a2c
Krzysztof-Vaari <krzysztof.vaari@gmail.com>
2016-11-13 18:29:40 +0100

    Adding containers

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { App } from './app/app';
+import { App } from './app';
+import { Main } from './app/containers';
 
 @NgModule({
     imports: [BrowserModule],
-    declarations: [App],
+    declarations: [App, Main],
     bootstrap: [App],
 })
 export class AppModule { };
