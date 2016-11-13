adcba28f67bc6e29e6f8be7202f92c80e0afcc95
Krzysztof-Vaari <krzysztof.vaari@gmail.com>
2016-11-13 22:50:34 +0100

    Adding notes container and first note

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { App } from './app';
-import { Main } from './app/containers';
-import { AppBar } from './app/ui';
+import { Main, NotesContainer } from './app/containers';
+import { AppBar, NoteCard } from './app/ui';
+
 
 @NgModule({
     imports: [BrowserModule],
-    declarations: [App, Main, AppBar],
+    declarations: [App, Main, AppBar, NoteCard, NotesContainer],
     bootstrap: [App],
 })
 export class AppModule { };
