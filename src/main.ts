69cf71faf908b50b237670664d6691cbae66e203
Krzysztof-Vaari <krzysztof.vaari@gmail.com>
2016-11-13 21:07:28 +0100

    Adding header with navbar

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { App } from './app';
 import { Main } from './app/containers';
+import { AppBar } from './app/ui';
 
 @NgModule({
     imports: [BrowserModule],
-    declarations: [App, Main],
+    declarations: [App, Main, AppBar],
     bootstrap: [App],
 })
 export class AppModule { };
