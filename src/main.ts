ef097d930e6c4462b87dc138ca192f9d5de76d0e
Krzysztof-Vaari <krzysztof.vaari@gmail.com>
2016-11-13 17:43:58 +0100

    first Angular2 Component

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { App } from './app/app';
+
+@NgModule({
+    imports: [BrowserModule],
+    declarations: [App],
+    bootstrap: [App],
+})
+export class AppModule { };
+
+platformBrowserDynamic().bootstrapModule(AppModule);
